15c19261ce9404228f7249b163eb787edda155a7
Emanuele Blanco <emanuele.blanco@moneyfarm.com>
2017-12-06 08:07:15 +0000

    And and Or

--- src/main/scala/Chapter10.scala
+++ src/main/scala/Chapter10.scala
@@ -1,4 +1,6 @@
 import cats._
+import cats.data.Validated
+import cats.data.Validated.{Invalid, Valid}
 import cats.implicits._
 
 object Chapter10 {
@@ -19,17 +21,23 @@ object Chapter10 {
   }
 
   sealed trait Check[E, A] {
+
     def and(that: Check[E, A]): Check[E, A] = And(this, that)
 
-    def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
+    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
       case Pure(func) => func(a)
 
       case And(left, right) =>
-        (left(a), right(a)) match {
-          case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
-          case (Left(e), _)         => e.asLeft
-          case (_, Left(e))         => e.asLeft
-          case (Right(_), Right(_)) => a.asRight
+        (left(a), right(a)).mapN((_, _) => a) // intelliJ doesn't like this
+
+      case Or(left, right) =>
+        left(a) match {
+          case Valid(_) => Valid(a)
+          case Invalid(e1) =>
+            right(a) match {
+              case Valid(_)    => Valid(a)
+              case Invalid(e2) => Invalid(e1 |+| e2)
+            }
         }
     }
   }
@@ -37,5 +45,11 @@ object Chapter10 {
   final case class And[E, A](left: Check[E, A], right: Check[E, A])
       extends Check[E, A]
 
-  final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
+  final case class Or[E, A](left: Check[E, A], right: Check[E, A])
+      extends Check[E, A]
+
+  final case class Pure[E, A](func: A => Validated[E, A]) extends Check[E, A]
+
+  // val a: Check[List[String], Int] = Pure { v => Validated.valid(v).ensure(List("must be > 2"))(_ > 2) }
+
 }
