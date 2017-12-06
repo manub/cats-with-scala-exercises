a303b4ba55f506d674beb8e971561079524d32bf
Emanuele Blanco <emanuele.blanco@moneyfarm.com>
2017-12-06 07:51:30 +0000

    Chapter 10 with ADT

--- src/main/scala/Chapter10.scala
+++ src/main/scala/Chapter10.scala
@@ -18,4 +18,24 @@ object Chapter10 {
       }
   }
 
+  sealed trait Check[E, A] {
+    def and(that: Check[E, A]): Check[E, A] = And(this, that)
+
+    def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
+      case Pure(func) => func(a)
+
+      case And(left, right) =>
+        (left(a), right(a)) match {
+          case (Left(e1), Left(e2)) => (e1 |+| e2).asLeft
+          case (Left(e), _)         => e.asLeft
+          case (_, Left(e))         => e.asLeft
+          case (Right(_), Right(_)) => a.asRight
+        }
+    }
+  }
+
+  final case class And[E, A](left: Check[E, A], right: Check[E, A])
+      extends Check[E, A]
+
+  final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
 }
