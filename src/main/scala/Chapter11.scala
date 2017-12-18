8c66044bbf0dff53f8de1d20f3b2a492eb80ff87
Emanuele Blanco <emanuele.blanco@gmail.com>
2017-12-18 22:46:17 +0000

    Finished chapter 11

--- src/main/scala/Chapter11.scala
+++ src/main/scala/Chapter11.scala
@@ -1,3 +1,6 @@
+import Chapter11.Exercise_11_3.BoundedSemiLattice
+import cats.kernel.Monoid
+
 object Chapter11 {
 
   object Exercise_11_2 {
@@ -46,16 +49,79 @@ object Chapter11 {
         val empty: Set[A] = Set.empty
       }
 
-    final case class GCounter[A](counters: Map[String, A])
-                                (implicit bsl: BoundedSemiLattice[A]) {
+    final case class GCounter[A](counters: Map[String, A]) {
 
-      import cats.syntax._
+      import cats.implicits._
 
-      def increment(machine: String, amount: A): GCounter[A] = ???
+      def increment(machine: String, amount: A)(
+          implicit m: Monoid[A]): GCounter[A] = {
+        val value = amount |+| counters.getOrElse(machine, m.empty)
+        GCounter(counters + (machine -> value))
+      }
 
-      def merge(that: GCounter[A]): GCounter[A] = ???
+      def merge(that: GCounter[A])(
+          implicit b: BoundedSemiLattice[A]): GCounter[A] =
+        GCounter(counters |+| that.counters)
 
-      def total: A = bsl.combineAll(counters.values)
+      def total(implicit m: Monoid[A]): A = m.combineAll(counters.values)
+    }
+  }
+
+  object Exercise_11_4 {
+
+    import cats.implicits._
+
+    trait GCounter[F[_, _], K, V] {
+      def increment(f: F[K, V])(k: K, v: V)(implicit m: Monoid[V]): F[K, V]
+
+      def merge(f1: F[K, V], f2: F[K, V])(
+          implicit b: BoundedSemiLattice[V]): F[K, V]
+
+      def total(f: F[K, V])(implicit m: Monoid[V]): V
+    }
+
+    object GCounter {
+      def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]) = counter
+    }
+
+    implicit def mapGCounter[K, V] = new GCounter[Map, K, V] {
+      override def increment(map: Map[K, V])(k: K, v: V)(
+          implicit m: Monoid[V]): Map[K, V] = {
+        val value = v |+| map.getOrElse(k, m.empty)
+        map + (k -> value)
+      }
+
+      override def merge(map1: Map[K, V], map2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+        map1 |+| map2
+
+      override def total(map: Map[K, V])(implicit m: Monoid[V]): V =
+        m.combineAll(map.values)
+    }
+  }
+  
+  object Exercise_11_5 {
+    
+    trait KeyValueStore[F[_, _]] {
+      def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+      
+      def get[K, V](f: F[K, V])(k: K): Option[V]
+      
+      def getOrElse[K, V](f: F[K, V])(k: K, default: => V): V =
+        get(f)(k).getOrElse(default)
+      
+      def values[K, V](f: F[K, V]): List[V]
+    }
+    
+    object KeyValueStore {
+      def apply[F[_, _]](implicit keyValueStore: KeyValueStore[F]) = keyValueStore
+    }
+    
+    implicit val mapKeyValueStore = new KeyValueStore[Map] {
+      def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] = f.updated(k, v)
+
+      def get[K, V](f: Map[K, V])(k: K): Option[V] = f.get(k)
+    
+      def values[K, V](f: Map[K, V]): List[V] = f.values.toList
     }
   }
 }
